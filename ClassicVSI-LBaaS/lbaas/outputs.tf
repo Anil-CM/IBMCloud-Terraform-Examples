d83b51d1029cd4d69999c7d81d3df5b586e15688
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2021-02-07 15:27:30 +0530

    Update outputs.tf

--- ClassicVSI-LBaaS/lbaas/outputs.tf
+++ ClassicVSI-LBaaS/lbaas/outputs.tf
@@ -6,6 +6,6 @@ output health_monitors {
   value = ibm_lbaas.lbaas.health_monitors
 }
 
-output "env_values" {
+output "schematics_workspace_id" {
     value = [for x in split(",", chomp(data.local_file.env_info.content)): split(":", x)[1] if contains(split(":", x), "Schematics")][0]
 }
