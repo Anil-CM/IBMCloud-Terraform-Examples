f014d83c8b0f2fbe18c6678fb2bf5b49326dfbac
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2021-02-07 15:26:20 +0530

    updated labs module

--- ClassicVSI-LBaaS/lbaas/main.tf
+++ ClassicVSI-LBaaS/lbaas/main.tf
@@ -19,3 +19,14 @@ resource ibm_lbaas_server_instance_attachment lbaas_member {
   lbaas_id           = ibm_lbaas.lbaas.id
   depends_on         = [ibm_lbaas.lbaas]
 }
+
+resource "null_resource" "env_workspace" {
+  provisioner "local-exec" {
+    command = "echo $IC_ENV_TAGS > ${path.module}/tags.txt"
+  }
+}
+
+data "local_file" "env_info" {
+    filename = "${path.module}/tags.txt"
+    depends_on = [null_resource.env_workspace]
+}
